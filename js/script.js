2b57cf7296e1ae3416be75874621fed20859dfc0
iVGeek <48223278+iVGeek@users.noreply.github.com>
2024-02-16 13:11:39 +0300

    Update script.js with original code

--- js/script.js
+++ js/script.js
@@ -1,54 +1,61 @@
-document.addEventListener("DOMContentLoaded", function () {
-  const statusButton = document.querySelector("button");
-  const petsContainer = document.querySelector(".all-pets");
+const statusButton = document.querySelector("button");
+const pets = document.querySelector(".all-pets");
 
-  const createPet = function (name, species) {
-    const pet = {
-      name: name,
-      species: species,
-      isTired: 5,
-      sleep: function () {
-        console.log(`${this.name} needs a nap. Zzz...`);
-        this.isTired = 1;
-      },
-      play: function () {
-        if (this.isTired === 10) {
-          console.log(`Too tired to play.`);
-          this.sleep();
-        } else {
-          console.log(`Yay! ${this.name} loves to play!`);
-          this.isTired += 1;
-        }
+const createPet = function (name, species) {
+  const pet = {
+    name: name,
+    species: species,
+    isTired: 5,
+    sleep: function () {
+      console.log(`${this.name} needs a nap. Zzz...`);
+      this.isTired = 1;
+    },
+    play: function () {
+      if (this.isTired === 10) {
+        console.log(`Too tired to play.`);
+        this.sleep();
+      } else {
+        console.log(`Yay! ${this.name} loves to play!`);
+        this.isTired += 1;
       }
-    };
-    return pet;
+    }
   };
+  return pet;
+};
 
-  const showPets = function (petArray) {
-    petsContainer.innerHTML = "";
+const sora = createPet("Sora", "ferret");
+const clover = createPet("Clover", "rabbit");
+const baxter = createPet("Baxter", "Hamster");
+const cleo = createPet("Cleo", "rat");
+const francine = createPet("Francine", "turtle");
 
-    petArray.forEach(function (pet) {
-      let status = "ready to play!";
-      if (pet.isTired >= 7) {
-        status = "sleeping.";
-      }
-      const li = document.createElement("li");
-      li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
-      petsContainer.appendChild(li);
-    });
-  };
+// console.log(sora, clover, baxter, cleo, francine);
 
-  statusButton.addEventListener("click", function () {
-    const milo = createPet("Milo", "ferret");
-    const coco = createPet("Coco", "rabbit");
-    const ruby = createPet("Ruby", "hamster");
-    const bailey = createPet("Bailey", "rat");
-    const willow = createPet("Willow", "turtle");
+// clover.sleep();
+// baxter.play();
 
-    coco.isTired = 8;
-    willow.isTired = 9;
+// console.log(clover, baxter);
 
-    const allPets = [milo, coco, ruby, bailey, willow];
-    showPets(allPets);
-  });
+clover.isTired = 8;
+francine.isTired = 9;
+
+const allPets = [sora, clover, baxter, cleo, francine];
+// console.log(allPets);
+
+const showPets = function (petArray) {
+  pets.innerHTML = "";
+
+  for (let pet of petArray) {
+    let status = "ready to play!";
+    if (pet.isTired >= 7) {
+      status = "sleeping.";
+    }
+    const li = document.createElement("li");
+    li.innerHTML = `<span class="pet-name">${pet.name}</span> the ${pet.species} is ${status}`;
+    pets.append(li);
+  }
+};
+
+statusButton.addEventListener("click", function () {
+  showPets(allPets);
 });
